a5a953113a9de543b12e725e25111dd8bb9afc9c
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-05-05 23:05:56 +0300

    Login form initialized

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
-
+const path = require("path");
 //import routes
 const dashboard = require("./routes/dashboard");
+const login = require("./routes/login");
+
+//Template engine
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.static(path.join(__dirname, "public"))); //set public dir for assests
 //use routes
 app.use("/", dashboard);
+app.use("/login", login);
 
 app.listen(20000, "localhost", () => {
   console.log("App running on port 20000");
--- routes/login.js
+++ routes/login.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.render("dashboard.pug");
+  res.render("login.pug");
 });
 
 module.exports = router;
