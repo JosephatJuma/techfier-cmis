ba9c2cd6c68ad0e8e5e61120838219918b333f49
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-05-05 23:17:25 +0300

    sidebar and navbar put in components

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+
 //import routes
 const dashboard = require("./routes/dashboard");
 const login = require("./routes/login");
@@ -9,6 +10,7 @@ const login = require("./routes/login");
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "public"))); //set public dir for assests
+
 //use routes
 app.use("/", dashboard);
 app.use("/login", login);
