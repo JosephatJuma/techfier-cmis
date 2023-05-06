8ec4466d03e81e0e09ea3096c71d5caaa7a8a2da
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-05-06 14:53:08 +0300

    fetch number of equipment from db

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const Equipment = require("../models/equipment");
 
-router.get("/", (req, res) => {
-  res.render("dashboard.pug", { page: "Dashboard" });
+router.get("/", async (req, res) => {
+  const equipments = await Equipment.find();
+
+  res.render("dashboard.pug", {
+    page: "Dashboard",
+    number_of_equipments: equipments.length,
+  });
 });
 
 module.exports = router;
