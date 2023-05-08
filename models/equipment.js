920e6b88516daf989d5c913083f76469f97a411a
Josephat10 <91407741+Josephat10@users.noreply.github.com>
2023-05-08 13:53:32 +0300

    Equipment list gets data from db

--- models/equipment.js
+++ models/equipment.js
@@ -1,8 +1,10 @@
 const mongoose = require("mongoose");
 
 const Equipment = new mongoose.Schema({
+  id: { type: String },
   equipment_name: { type: String },
   department: { type: String },
+  barcode: { type: String },
 });
 
 module.exports = mongoose.model("equipment", Equipment);
--- routes/equpment.js
+++ routes/equpment.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = express.Router();
 const Equipment = require("../models/equipment");
 
-router.get("/", (req, res) => {
-  res.send("equipment");
+router.get("/", async (req, res) => {
+  const equipment_list = await Equipment.find();
+  res.render("equipment.pug", {
+    page: "All Equipment",
+    equipment_list: equipment_list,
+  });
 });
 
 router.get("/add", (req, res) => {
